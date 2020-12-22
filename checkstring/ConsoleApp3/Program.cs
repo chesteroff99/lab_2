3a07a183011bc0baf1a2d0be682f9250aa2c8ad4
chesterooff99 <chersteroff99@mail.ru>
2020-12-22 15:48:25 +0300

    проверка сложения

---
+++ checkstring/ConsoleApp3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp3
+{
+
+        class Program
+        {
+            static void Main(string[] args)
+        {
+            bool check = true;
+            double a = 0, b = 0, c = 0;
+               
+                while (check == true)
+                {
+                    Console.WriteLine("1.Сложение");
+                    Console.WriteLine("2.Вычитание");
+                    Console.WriteLine("3.Умножение");
+                    Console.WriteLine("4.Деление");
+
+                    switch (Console.ReadLine())
+                    {
+
+                        case "1"://сложение
+                            ReadParams();
+                        if ((a == 0) && (b == 0)){Console.WriteLine("Вы ввели нули"); }
+                        else
+                            c = a + b;
+                            Console.WriteLine(a + "+" + b + "=" + c);
+                            break;
+
+                        case "2"://вычит
+                            ReadParams();
+                            c = a - b;
+                            Console.WriteLine(a + "-" + b + "=" + c);
+
+                            break;
+
+                        case "3"://умнож
+                            ReadParams();
+                            c = a * b;
+                            Console.WriteLine(a + "*" + b + "=" + c);
+
+                            break;
+
+                        case "4"://дел
+                            ReadParams();
+                            c = a / b;
+                            Console.WriteLine(a + "/" + b + "=" + c);
+                            break;
+
+
+                        default:
+                            Console.WriteLine("Нет такой операции!");
+                            Console.WriteLine();
+                            break;
+
+                    }
+
+                    void ReadParams()
+                    {
+                        Console.WriteLine("a:");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("b:");
+                        b = Convert.ToDouble(Console.ReadLine());
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+   
+
